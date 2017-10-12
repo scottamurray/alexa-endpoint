9e4e96638a6067f8a900f709fe19db7ea1d08c99
Scott Anthony Murray <what.even.are.monads@gmail.com>
2017-10-11 22:21:29 -0400

    Deleted changelog, /resources, /doc, /test; started on display directives

--- src/alexa/directives/display.clj
+++ src/alexa/directives/display.clj
@@ -2,4 +2,26 @@
 
 (ns alexa.directives.display)
 
-(defn render-template "http://amzn.to/2gaqmBu" [] {})
+(defn render-template
+  "Returns a directive telling Alexa to render content to the app screen.
+  For more, see http://amzn.to/2gaqmBu."
+  []
+  {:type "Display.RenderTemplate"})
+
+(defn using-full-width-text-template
+  "Applies the full-width text template to the given RenderTemplate directive."
+  [directive token title text]
+  (assoc directive :template {:type "BodyTemplate1"
+                              :token token
+                              :title title
+                              :text-content text}))
+
+(defn using-image-left-template [] {})
+
+(defn using-image-right-template [] {})
+
+(defn using-full-screen-image-overlay-template [] {})
+
+(defn using-vertical-list-template [] {})
+
+(defn using-horizontal-list-with-image-template [] {})
